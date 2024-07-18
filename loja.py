1980b70d3ca1a74f98a87350cb4820ecef579bf8
oscandido10@gmail.com <oscandido10@gmail.com>
2024-07-18 07:49:52 -0300

    implemented the classes Produto and Pedido

---
+++ loja.py
@@ -0,0 +1,53 @@
+class Produto:
+    def __init__(self, id, nome, descricao, preco) -> None:
+        self.__id = id
+        self.__nome = nome
+        self.__descricao = descricao
+        self.__preco = preco
+        self.__estoque = 0
+
+    def get_id(self):
+        return self.__id
+    
+    def get_nome(self):
+        return self.__nome
+    
+    def set_nome(self, nome):
+        self.__nome = nome
+    
+    def get_descricao(self):
+        return self.__descricao
+    
+    def set_descricao(self, descricao):
+        self.__descricao = descricao
+
+    def get_preco(self):
+        return self.__preco
+    
+    def set_preco(self, preco):
+        self.__preco = preco
+
+    def get_estoque(self):
+        return self.__estoque
+    
+    def set_estoque(self, estoque):
+        self.__estoque = estoque
+
+class Pedido:
+    def __init__(self, id, produto, quantidade):
+        self.__id = id
+        self.__produto = produto
+        self.__quantidade = quantidade
+    
+    def consultar_preco(self):
+        return self.__produto.get_preco() * self.__quantidade
+
+    def get_id(self):
+        return self.__id
+    
+    def get_produto(self):
+        return self.__produto
+    
+    def get_quantidade(self):
+        return self.__quantidade
+    
